030991025b04c5f142139ca0c487b98b273a944a
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-05-29 20:17:16 +0530

    Time: 648 ms (13.07%), Space: 118.3 MB (6.44%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,20 +1,25 @@
 class Solution {
     public int maxProduct(String[] words) {
         int max = 0;
-        for(int i =0;  i<words.length; i++){
-            for(int j = i+1; j < words.length; j++){
-                if(!isMatch(words[i], words[j]))
-                    max = max > words[i].length() * words[j].length() ? max : words[i].length() * words[j].length();
+        int len = words.length;
+        for(int i =0;  i < len; i++){
+            for(int j = i+1; j < len; j++){
+                if(!isMatch(words[i], words[j])){
+                    int mul = words[i].length() * words[j].length();
+                    max = max > mul ? max : mul;
+                }
             }
         }
         return max;
     }
     boolean isMatch(String a, String b){
         char[] chs = new char[26];
-        for(int i =0; i< a.length(); i++){
+        int aLen = a.length();
+        for(int i =0; i< aLen; i++){
             chs[a.charAt(i) - 'a']++;
         }
-        for(int i =0; i<b.length(); i++){
+        int bLen = b.length();
+        for(int i =0; i<bLen; i++){
             if(chs[b.charAt(i) - 'a'] > 0)
                 return true;
         }
