b1b2ef9eb728231e50679adf6fa077d5b4a43c8e
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-06-27 11:50:21 +0530

    Time: 903 ms (85.58%), Space: 26.5 MB (84.63%) - LeetHub

--- 848-shifting-letters/848-shifting-letters.py
+++ 848-shifting-letters/848-shifting-letters.py
@@ -1,7 +1,8 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[int]) -> str:
+        arr = [i for i in s]
         for i in range(len(shifts) - 2 , -1, -1):
             shifts[i] += shifts[i+1]
-        for i in range(len(s)):
-            s = s[:i] + chr((((ord(s[i]) + shifts[i]) - ord('a'))%26) + ord('a')) + s[i + 1:] 
-        return s
+        for i in range(len(arr)):
+            arr[i] =  chr((((ord(arr[i]) + shifts[i]) - ord('a'))%26) + ord('a')) 
+        return "".join(arr)
