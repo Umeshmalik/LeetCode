bb933c6f63309335df0d6ee85e24a19172593963
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-03 10:41:47 +0530

    Time: 94 ms (35.19%), Space: 13.9 MB (75.30%) - LeetHub

--- 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
+++ 80-remove-duplicates-from-sorted-array-ii/80-remove-duplicates-from-sorted-array-ii.py
@@ -1,26 +1,11 @@
 class Solution:
-    def removeDuplicates(self, arr: List[int]) -> int:
-        sl, ft, ch, num = 1, 1, 1, arr[0]
-        while ft < len(arr):
-            if arr[ft] == num:
-                ch += 1
-                arr[sl] = arr[ft]
-                sl += 1
-                ft += 1
-                if ch == 2 and ft < len(arr):
-                    while ft < len(arr) and arr[ft] == num:
-                        ft += 1
-                    if ft == len(arr):
-                        return sl
-                    ch = 1
-                    num = arr[ft]
-                    arr[sl] = num
-                    sl += 1
-                    ft += 1
+    def removeDuplicates(self, nums: List[int]) -> int:
+        left = right = 2
+        while right < len(nums):
+            if nums[right] == nums[left-2]:
+                right += 1
             else:
-                ch = 1
-                num = arr[ft]
-                arr[sl] = num
-                sl += 1
-                ft += 1
-        return sl
+                nums[left] = nums[right]
+                left += 1
+                right += 1
+        return left
