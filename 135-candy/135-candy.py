e4b89f4591fe9860927a32cbc4f9402f3516fdf1
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-05 00:58:21 +0530

    Time: 325 ms (19.66%), Space: 16.7 MB (74.92%) - LeetHub

--- 135-candy/135-candy.py
+++ 135-candy/135-candy.py
@@ -5,10 +5,8 @@ class Solution:
         arr2 = [1] * N
         for i in range(1, N):
             arr1[i] = arr1[i-1] + 1 if ratings[i] > ratings[i-1] else 1
-        
-        for i in range(N-2, -1, -1):
-            arr2[i] = arr2[i+1] + 1 if ratings[i] > ratings[i+1] else 1
-    
+            arr2[N - i - 1] = arr2[N - i] + 1 if ratings[ N - i - 1] > ratings[N - i] else 1
+
         summ = 0
         for i in range(N):
             summ += max(arr1[i], arr2[i])
