b71476b1bc20eed964ad77b1cc57c108eebf7e9a
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-05 00:52:52 +0530

    Time: 361 ms (11.03%), Space: 16.9 MB (18.65%) - LeetHub

---
+++ 135-candy/135-candy.py
@@ -0,0 +1,15 @@
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        N = len(ratings)
+        arr1 = [1] * N
+        arr2 = [1] * N
+        for i in range(1, N):
+            arr1[i] = arr1[i-1] + 1 if ratings[i] > ratings[i-1] else 1
+        
+        for i in range(N-2, -1, -1):
+            arr2[i] = arr2[i+1] + 1 if ratings[i] > ratings[i+1] else 1
+    
+        summ = 0
+        for i in range(N):
+            summ += max(arr1[i], arr2[i])
+        return summ
