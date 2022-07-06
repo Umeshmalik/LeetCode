5d45052e538ab74d668a2243758fae80d5b0e483
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-06 09:13:22 +0530

    Time: 64 ms (33.95%), Space: 13.9 MB (54.45%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.py
+++ 509-fibonacci-number/509-fibonacci-number.py
@@ -2,9 +2,6 @@ class Solution:
     def fib(self, n: int) -> int:
         first = second = 1
         if n == 0 : return 0
-        if n == 1 or n == 2: return 1
         for i in range(2, n):
-            temp = second
-            second += first
-            first = temp
+            second , first = first + second , second
         return second
