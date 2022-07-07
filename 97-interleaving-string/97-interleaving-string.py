88e45ff26f4dcffffe1155841f7645722d46698c
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-07 22:58:11 +0530

    Time: 90 ms (20.61%), Space: 14.2 MB (58.75%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.py
+++ 97-interleaving-string/97-interleaving-string.py
@@ -1,15 +1,19 @@
 class Solution:
     def isInterleave(self, s1: str, s2: str, s3: str) -> bool:
-        d = {}
-        def isInterleaving(X, Y, S, T):
-            if not X and not Y and not S:
-                return True
-            if not S:
+        def isInterleaving(X, Y, S):
+            M, N = len(X), len(Y)
+            if M + N != len(S):
                 return False
-            key = (X, Y, S)
-            if key not in T:
-                x = (len(X) and S[0] == X[0]) and isInterleaving(X[1:], Y, S[1:], T)
-                y = (len(Y) and S[0] == Y[0]) and isInterleaving(X, Y[1:], S[1:], T)
-                T[key] = x or y
-            return T[key]
-        return isInterleaving(s1, s2, s3, d)
+            T = [[False for x in range(N + 1)] for y in range(M + 1)]
+            for i in range(0, M + 1):
+                for j in range(0, N + 1):
+                    if i == 0 and j == 0:
+                        T[i][j] = True
+                    elif i and j and X[i - 1] == S[i + j - 1] and Y[j - 1] == S[i + j - 1]:
+                        T[i][j] = T[i - 1][j] or T[i][j - 1]
+                    elif i and X[i - 1] == S[i + j - 1]:
+                        T[i][j] = T[i - 1][j]
+                    elif j and Y[j - 1] == S[i + j - 1]:
+                        T[i][j] = T[i][j - 1]
+            return T[M][N]
+        return isInterleaving(s1, s2, s3)
