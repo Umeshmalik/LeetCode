7d6ff9b73a0ce76074518490fd4bbecb44cd7df3
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-07-11 17:45:59 +0530

    Time: 32 ms (94.95%), Space: 13.8 MB (69.82%) - LeetHub

---
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.py
@@ -0,0 +1,20 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        d = {}
+        self.helper(root, d, 0)
+        return [d[i] for i in d]
+    
+    def helper(self, root, d, level):
+        if not root:
+            return
+        if level not in d:
+            d[level] = root.val
+        self.helper(root.right, d, level+1)
+        self.helper(root.left, d, level+1)
+        return
