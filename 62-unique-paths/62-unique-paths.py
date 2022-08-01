8b81671106462012b3905ad18a95c56f60fd6a18
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-08-01 17:15:42 +0530

    Time: 47 ms (57.79%), Space: 13.9 MB (73.91%) - LeetHub

--- 62-unique-paths/62-unique-paths.py
+++ 62-unique-paths/62-unique-paths.py
@@ -1,14 +1,6 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        arr = []
-        for i in range(m):
-            ar = []
-            for j in range(n):
-                if i == 0 or j == 0:
-                    ar.append(1)
-                else:
-                    ar.append(0)
-            arr.append(ar)
+        arr = [[1]*n]*m
         for i in range(1, m):
             for j in range(1, n):
                 arr[i][j] = arr[i][j-1] + arr[i-1][j]
