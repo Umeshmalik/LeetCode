735db8fb4c779f494a77ae236970718838015eff
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-08-05 16:10:36 +0530

    Time: 1172 ms (46.89%), Space: 19.1 MB (28.54%) - LeetHub

---
+++ 997-find-the-town-judge/997-find-the-town-judge.py
@@ -0,0 +1,17 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        if len(trust) == 0 and n == 1: return 1
+        d = {}
+        for i, j in trust:
+            if j in d:
+                d[j] += 1
+            else:
+                d[j] = 1
+        peo = []
+        for i in d.keys():
+            if d[i] == n - 1:
+                peo.append(i)
+        for i, j in trust:
+            if i in peo:
+                peo.remove(i)
+        return -1 if len(peo) == 0 else peo[0]
