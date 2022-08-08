534e7ec7412db4fde31a39c8a2580a4047aabd48
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-08-08 20:41:22 +0530

    Time: 1339 ms (8.61%), Space: 24.7 MB (45.51%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.py
@@ -0,0 +1,19 @@
+class Solution:
+    def increasingTriplet(self, arr: List[int]) -> bool:
+        def bisect_left(list, n):
+            lo = 0
+            hi = len(list)
+            while lo < hi:
+                mid = (lo + hi) >> 1
+                if list[mid] < n:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            return lo
+        dp = [arr[0]]
+        for i, n in enumerate(arr[1:]):
+            if dp[-1] < n:
+                dp.append(n)
+            else:
+                dp[bisect_left(dp, n)] = n
+        return False if len(dp) < 3 else True
