a44f0a8a8f315aa4e1242e3c28b97ad75a187236
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-08-08 12:59:28 +0530

    Time: 870 ms (78.03%), Space: 26.2 MB (99.02%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.py
+++ 11-container-with-most-water/11-container-with-most-water.py
@@ -3,9 +3,10 @@ class Solution:
         mx , lf, rg = 0, 0, len(height)-1
         while lf < rg:
             if height[lf] < height[rg]:
-                mx = max(mx, height[lf]*(rg-lf))
+                res = height[lf]*(rg-lf)
                 lf += 1
             else:
-                mx = max(mx, height[rg]*(rg-lf))
+                res = height[rg]*(rg-lf)
                 rg -= 1
+            if res > mx: mx = res
         return mx
