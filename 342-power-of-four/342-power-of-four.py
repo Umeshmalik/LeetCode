04f2b015cff673f606f5c194b250973bd5efa580
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-08-22 14:33:10 +0530

    Time: 62 ms (22.69%), Space: 13.9 MB (10.18%) - LeetHub

--- 342-power-of-four/342-power-of-four.py
+++ 342-power-of-four/342-power-of-four.py
@@ -1,14 +1,3 @@
 class Solution:
     def isPowerOfFour(self, n: int) -> bool:
-        if n < 0: return False
-        bn = bin(n)
-        one = False
-        zero = 0
-        for i in range(len(bn)):
-            if one and bn[i] == '0':
-                zero += 1
-            if one and bn[i] == '1':
-                return False
-            if not one and bn[i] == '1':
-                one = True
-        return True if one and zero % 2 == 0 else False
+        return (n & (n-1)) == 0 and n > 0 and (n-1) % 3 == 0
