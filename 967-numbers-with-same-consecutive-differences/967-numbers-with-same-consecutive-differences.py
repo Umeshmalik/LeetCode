3eb9daab907bea45cb0e60ecefef282b523d5a41
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-03 11:22:21 +0530

    Time: 384 ms (5.10%), Space: 14.4 MB (21.77%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -2,7 +2,7 @@ class Solution:
     def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
         self.arr = set()
         def get(st, nxt):
-            if nxt > 9 or nxt < 0 or len(st) > n: return
+            if nxt > 9 or nxt < 0 or len(st) > n: return None
             st = st + str(nxt)
             if len(st) == n: self.arr.add(st)
             get(st, nxt-k)
