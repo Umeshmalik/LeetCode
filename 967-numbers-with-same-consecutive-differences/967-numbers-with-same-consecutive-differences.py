8eb83583c451d3e0b7646a93a2433c0a32045511
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-03 11:16:18 +0530

    Time: 353 ms (5.10%), Space: 14.5 MB (12.93%) - LeetHub

---
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.py
@@ -0,0 +1,14 @@
+class Solution:
+    def numsSameConsecDiff(self, n: int, k: int) -> List[int]:
+        self.arr = set()
+        def get(st, nxt):
+            if nxt > 9 or nxt < 0 or len(st) > n: return
+            st = st + str(nxt)
+            if len(st) == n: self.arr.add(st)
+            get(st, nxt-k)
+            get(st, nxt+k)
+            return None
+        for i in range(1, 10):
+            for j in range(n):
+                get("", i)
+        return self.arr
