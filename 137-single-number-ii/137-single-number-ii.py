56563ef8b72e3ac3a23aa828a25a449ed7449240
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-06 21:40:30 +0530

    Time: 106 ms (50.34%), Space: 16.1 MB (8.16%) - LeetHub

--- 137-single-number-ii/137-single-number-ii.py
+++ 137-single-number-ii/137-single-number-ii.py
@@ -1,15 +1,19 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        res = [0]*32
-        negCount = 0
-        for i in nums:
-            s = bin(i)[2:] if bin(i)[0] != '-' else bin(i)[3:]
-            if bin(i)[0] == '-':
-                negCount += 1
-            for j in range(len(s)-1, -1, -1):
-                res[31 - len(s) + j + 1] += int(s[j])
-        ans = 0
-        for i in range(32):
-            if res[31- i] % 3 == 1:
-                ans += (2 ** i)
-        return -ans if negCount%3 == 1 else ans
+        # res = [0]*32
+        # negCount = 0
+        # for i in nums:
+        #     s = bin(i)[2:] if bin(i)[0] != '-' else bin(i)[3:]
+        #     if bin(i)[0] == '-':
+        #         negCount += 1
+        #     for j in range(len(s)-1, -1, -1):
+        #         res[31 - len(s) + j + 1] += int(s[j])
+        # ans = 0
+        # for i in range(32):
+        #     if res[31- i] % 3 == 1:
+        #         ans += (2 ** i)
+        # return -ans if negCount%3 == 1 else ans
+        d = collections.Counter(nums)
+        for i in d.keys():
+            if d[i] == 1: return i
+        return None
