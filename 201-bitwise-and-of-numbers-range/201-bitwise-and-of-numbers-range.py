6be3f0d30e07a94470a7c89acfeea28aab7f71bb
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-08 18:41:01 +0530

    Time: 100 ms (45.04%), Space: 13.9 MB (68.63%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.py
@@ -1,15 +1,11 @@
 class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
-        if left == right: return left
-        countA = countB = 0
-        A, B = left, right
-        while A:
-            A >>= 1
-            countA += 1
-        while B:
-            B >>= 1
-            countB += 1
-        if countA != countB: return 0
-        res = left
-        for i in range(left+1, right+1): res &= i
-        return res
+        binA, binB = bin(left)[2:], bin(right)[2:]
+        if len(binA) != len(binB): return 0
+        res = ""
+        for i in range(len(binA)):
+            if binA[i] == binB[i]: res += binA[i]
+            else:
+                res += ("0" * (len(binA) - i))
+                break
+        return int(res, 2)
