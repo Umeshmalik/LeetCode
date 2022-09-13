7c4db0110d5644c73e9858f32ee37f5ed4e1f368
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-13 20:45:51 +0530

    Time: 129 ms (84.80%), Space: 14.2 MB (70.76%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.py
+++ 393-utf-8-validation/393-utf-8-validation.py
@@ -1,5 +1,6 @@
 class Solution:
     def validUtf8(self, data: List[int]) -> bool:
+
         def ones(n):
             for i, num in enumerate([128, 192, 224, 240, 248, 256]):
                 if n < num: return i
@@ -8,23 +9,21 @@ class Solution:
         while i < len(data):
             curr = ones(data[i])
             if curr > 4 or curr == 1: return False
-            if curr == 4 and i + 3 >= len(data):
+            
+            if curr == 4 and i + curr - 1 >= len(data):
                 return False
             elif curr == 4 and i + 3 < len(data):
-                ans = ones(data[i+1]) + ones(data[i+2]) + ones(data[i+3])
-                if ans != 3: return False
+                if ones(data[i+1]) + ones(data[i+2]) + ones(data[i+3]) != 3: return False
                 i += 3
             elif curr == 3 and i + 2 >= len(data):
                 return False
             elif curr == 3 and i + 2 < len(data):
-                ans = ones(data[i+1]) + ones(data[i+2])
-                if ans != 2: return False
+                if ones(data[i+1]) + ones(data[i+2]) != 2: return False
                 i += 2
             elif curr == 2 and i + 1 >= len(data):
                 return False
             elif curr == 2 and i + 1 < len(data):
-                ans = ones(data[i+1])
-                if ans != 1: return False
+                if ones(data[i+1]) != 1: return False
                 i += 1
             i += 1
         return True
