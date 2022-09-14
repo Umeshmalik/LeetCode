74e5542e8b9ce88301e65d3d1a0f8dee429a5e28
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-14 12:55:32 +0530

    Time: 1885 ms (14.33%), Space: 86.4 MB (19.31%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.py
@@ -7,17 +7,15 @@
 class Solution:
     def pseudoPalindromicPaths (self, root: Optional[TreeNode]) -> int:
         def check(node, memory, num):
+            if not node: return num
 
             memory ^= 1 << node.val
 
             if node.left is None and node.right is None:
                 if memory==0 or memory&(memory-1)==0:
                     num += 1
-            else:
-                if node.left:
-                    num = check(node.left, memory, num)
-                if node.right:
-                    num = check(node.right, memory, num)
+            num = check(node.left, memory, num)
+            num = check(node.right, memory, num)
                     
             return num
 
