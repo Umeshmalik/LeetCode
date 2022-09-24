d8f5b70975c1a969d87dcbe55dc510ebccb51acd
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-24 11:26:15 +0530

    Time: 85 ms (34.41%), Space: 15.5 MB (72.84%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -8,18 +8,18 @@ class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
         ans = []
         
-        def helper(node, st, sm):
+        def helper(node, arr, sm):
             if not node: return None
             sm += node.val
-            st += ("" if not st else ",") + str(node.val)
+            arr.append(node.val)
             if sm == targetSum and not node.right and not node.left:
                 nonlocal ans
-                ans.append(list(map(int, st.split(","))))
-            helper(node.right, st, sm)
-            helper(node.left, st, sm)
-            st = st[0:len(st)-len(str(node.val))]
+                ans.append([*arr])
+            helper(node.right, arr, sm)
+            helper(node.left, arr, sm)
+            arr.pop()
             sm -= node.val
             return None
         
-        helper(root, "", 0)
+        helper(root, [], 0)
         return ans
