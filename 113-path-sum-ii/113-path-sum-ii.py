f0367429b014811e13b3f507986448bb23ce09c9
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-09-24 11:14:46 +0530

    Time: 80 ms (43.07%), Space: 15.6 MB (72.84%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.py
+++ 113-path-sum-ii/113-path-sum-ii.py
@@ -6,17 +6,20 @@
 #         self.right = right
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> List[List[int]]:
-        self.arr = []
-        self.targetSum = targetSum
-        self.helper(root, 0, [])
-        return self.arr
-    
-    def helper(self, node, curr_sum, arr1):
-        if not node: return
-        new_arr = [i for i in arr1]
-        new_arr.append(node.val)
-        curr_sum += node.val
-        if curr_sum == self.targetSum and not node.left and not node.right: self.arr.append(new_arr)
-        if node.left: self.helper(node.left, curr_sum, new_arr)
-        if node.right: self.helper(node.right, curr_sum, new_arr)
-        return
+        ans = []
+        
+        def helper(node, arr, sm):
+            if not node: return None
+            sm += node.val
+            arr.append(node.val)
+            if sm == targetSum and not node.right and not node.left:
+                nonlocal ans
+                ans.append([*arr])
+            helper(node.right, arr, sm)
+            helper(node.left, arr, sm)
+            arr.pop()
+            sm -= node.val
+            return None
+        
+        helper(root, [], 0)
+        return ans
