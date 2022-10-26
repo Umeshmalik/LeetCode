77a9264e8e59823edce6366479cb90d46c07fd71
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-10-26 13:17:20 +0530

    Time: 57 ms (62.57%), Space: 13.8 MB (98.47%) - LeetHub

--- 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
+++ 1275-find-winner-on-a-tic-tac-toe-game/1275-find-winner-on-a-tic-tac-toe-game.py
@@ -1,8 +1,7 @@
 class Solution:
     def tictactoe(self, moves: List[List[int]]) -> str:
         arr = [["" for j in range(3)] for i in range(3)]
-        for i, [x, y] in enumerate(moves):
-            arr[x][y] = 'O' if i&1 else 'X'
+        for i, [x, y] in enumerate(moves): arr[x][y] = 'O' if i&1 else 'X'
         if arr[0][0] == arr[0][1] and arr[0][1] == arr[0][2] and arr[0][0]: return "A" if arr[0][0] == 'X' else 'B'
         if arr[0][0] == arr[1][1] and arr[1][1] == arr[2][2] and arr[0][0]: return "A" if arr[0][0] == 'X' else 'B'
         if arr[0][0] == arr[1][0] and arr[1][0] == arr[2][0] and arr[0][0]: return "A" if arr[0][0] == 'X' else 'B'
@@ -13,5 +12,5 @@ class Solution:
         if arr[2][0] == arr[2][1] and arr[2][1] == arr[2][2] and arr[2][0]: return "A" if arr[2][2] == 'X' else 'B'
         for i in range(3):
             for j in range(3):
-                if arr[i][j] == "": return "Pending"
+                if not arr[i][j]: return "Pending"
         return "Draw"
