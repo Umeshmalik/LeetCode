a97e91c85c57eb7e7b01be0cd3866ea77fd6625f
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-02 21:12:15 +0530

    Time: 228 ms (86.40%), Space: 14.8 MB (47.28%) - LeetHub

--- 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
+++ 1656-design-an-ordered-stream/1656-design-an-ordered-stream.py
@@ -1,13 +1,14 @@
 class OrderedStream:
 
     def __init__(self, n: int):
-        self.map = {}
+        self.map = [None] * n
         self.pointer = 0
+        self.n = n
 
     def insert(self, idKey: int, value: str) -> List[str]:
         self.map[idKey-1] = value
         res = []
-        while self.pointer in self.map:
+        while self.pointer < self.n and self.map[self.pointer]:
             res.append(self.map[self.pointer])
             self.pointer += 1
         return res
