739dd3f80dd380ee11106fead7f36ba95988a453
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-05 23:06:56 +0530

    Time: 1595 ms (59.87%), Space: 43.9 MB (49.32%) - LeetHub

--- 2043-simple-bank-system/2043-simple-bank-system.py
+++ 2043-simple-bank-system/2043-simple-bank-system.py
@@ -2,11 +2,9 @@ class Bank:
 
     def __init__(self, balance: List[int]):
         self.accounts = balance
-        
 
     def transfer(self, account1: int, account2: int, money: int) -> bool:
-        if account1 < 1 or len(self.accounts) < account1 or account2 < 1 or len(self.accounts) < account2: return False
-        if self.accounts[account1-1] < money: return False
+        if account1 < 1 or len(self.accounts) < account1 or account2 < 1 or len(self.accounts) < account2 or self.accounts[account1-1] < money: return False
         self.accounts[account1-1] -= money
         self.accounts[account2-1] += money
         return True
