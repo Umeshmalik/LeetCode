6a51a0cca1386745add43b9c4326d9ef3bc04e92
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-12 20:30:20 +0530

    Time: 145 ms (70.13%), Space: 23.4 MB (79.87%) - LeetHub

--- 0295-find-median-from-data-stream/0295-find-median-from-data-stream.rs
+++ 0295-find-median-from-data-stream/0295-find-median-from-data-stream.rs
@@ -1,42 +1,26 @@
+use std::collections::*;
+
 struct MedianFinder {
-    arr: Vec<i32>
+    f:BinaryHeap<i32>,
+    s:BinaryHeap<i32>
 }
 
-
-/** 
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
 impl MedianFinder {
 
     fn new() -> Self {
-        return Self {
-            arr: vec![]
-        }
+        Self{f:BinaryHeap::new(), s:BinaryHeap::new()}
     }
     
     fn add_num(&mut self, num: i32) {
-        self.arr.push(num);
+        if self.f.len()==0 {self.f.push(num); return;}
+        if *self.f.peek().unwrap()<=num {self.s.push(-num);} else {self.f.push(num);}
+        if self.s.len()>self.f.len() { self.f.push(-self.s.pop().unwrap());}
+        if self.s.len()+1<self.f.len() { self.s.push(-self.f.pop().unwrap());}
     }
     
-    fn find_median(&mut self) -> f64 {
-        let size : usize = self.arr.len();
-        self.arr.sort();
-        let ans : f64;
-        if size&1 == 1 {
-            ans = self.arr[size>>1] as f64;
-        }else{
-            let n1 : f64 = self.arr[size>>1] as f64;
-            let n2 : f64 = self.arr[(size>>1)-1] as f64;
-            ans = ((n1 + n2) / 2.0) as f64;
-        }
-        return ans;
+    fn find_median(&self) -> f64 {
+        let mut ans = *self.f.peek().unwrap() as f64;
+        if self.f.len()==self.s.len() {ans=(ans-*self.s.peek().unwrap() as f64)/2.0;}
+        ans
     }
 }
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * let obj = MedianFinder::new();
- * obj.add_num(num);
- * let ret_2: f64 = obj.find_median();
- */
