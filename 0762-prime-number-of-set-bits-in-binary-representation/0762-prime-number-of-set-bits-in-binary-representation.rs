6733e11a4842473eb24b3593afcf5f657b550010
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-12 23:14:10 +0530

    Time: 2 ms (93.75%), Space: 2 MB (68.75%) - LeetHub

--- 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.rs
+++ 0762-prime-number-of-set-bits-in-binary-representation/0762-prime-number-of-set-bits-in-binary-representation.rs
@@ -1,6 +1,6 @@
 impl Solution {
     pub fn count_prime_set_bits(left: i32, right: i32) -> i32 {
-        let primes : Vec<i32> = vec![2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31];
+        let primes : Vec<i32> = vec![2, 3, 5, 7, 11, 13, 17, 19];
         let mut ans : i32 = 0;
         for i in left..right+1{
             let c : i32 = i.count_ones() as i32;
