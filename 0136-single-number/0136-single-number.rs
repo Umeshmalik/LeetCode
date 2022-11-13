dcbb0536597acd66beb90ab7b7c95181388459e2
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-13 12:47:05 +0530

    Time: 0 ms (100.00%), Space: 2.2 MB (58.21%) - LeetHub

--- 0136-single-number/0136-single-number.rs
+++ 0136-single-number/0136-single-number.rs
@@ -1,5 +1,7 @@
 impl Solution {
     pub fn single_number(nums: Vec<i32>) -> i32 {
-        nums.into_iter().reduce(|a, b| a ^ b).unwrap()
+        let mut n = 0;
+        for i in nums{ n ^= i; }
+        n
     }
 }
