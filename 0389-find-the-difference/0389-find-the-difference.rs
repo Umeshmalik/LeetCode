10a131cae9ac6bd371996b3d478d97410ee3de8e
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-15 10:15:22 +0530

    Time: 1 ms (72.31%), Space: 2.3 MB (24.62%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.rs
+++ 0389-find-the-difference/0389-find-the-difference.rs
@@ -1,7 +1,7 @@
 impl Solution {
     pub fn find_the_difference(s: String, t: String) -> char {
-        let mut vec1 : [i32;32]  = [0; 32];
-        let mut vec2 : [i32;32]  = [0; 32];
+        let mut vec1 : [i32;26]  = [0; 26];
+        let mut vec2 : [i32;26]  = [0; 26];
         for i in s.bytes(){ vec1[(i-97) as usize] += 1; }
         for i in t.bytes(){ vec2[(i-97) as usize] += 1; }
         for i in 0..26{
