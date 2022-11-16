6b121f9b2b36fae287f81f48b3c9dac9bcbb9890
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-16 23:10:33 +0530

    Time: 1 ms (69.42%), Space: 2 MB (93.69%) - LeetHub

--- 0278-first-bad-version/0278-first-bad-version.rs
+++ 0278-first-bad-version/0278-first-bad-version.rs
@@ -4,20 +4,21 @@
 
 impl Solution {
     pub fn first_bad_version(&self, n: i32) -> i32 {
-		let mut low: i64 = 0;
-        let mut high: i64 = n as i64;
-        while low <= high{
-            let mid : i64 = (low+high)/2;
-            let fi = self.isBadVersion(mid as i32);
-            let se = self.isBadVersion(mid as i32 - 1);
-            if se == false && fi == true{
-                return mid as i32;
-            }else if fi == true && se == true{
-                high = mid - 1;
-            }else{
-                low = mid + 1;
-            }
-        }
-        0
+        self.____bad____
+		// let mut low: i64 = 0;
+		// let mut high: i64 = n as i64;
+		// while low <= high{
+		// let mid : i64 = (low+high)/2;
+		// let fi = self.isBadVersion(mid as i32);
+		// let se = self.isBadVersion(mid as i32 - 1);
+		// if se == false && fi == true{
+		// return mid as i32;
+		// }else if fi == true && se == true{
+		// high = mid - 1;
+		// }else{
+		// low = mid + 1;
+		// }
+		// }
+		// 0
     }
 }
