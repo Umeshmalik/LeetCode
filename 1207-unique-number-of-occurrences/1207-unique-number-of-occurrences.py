35f3ba64e1482f615bb5bcc325707e54ef0e4778
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2022-11-30 14:20:17 +0530

    Time: 60 ms (69.50%), Space: 14.1 MB (34.11%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.py
@@ -1,9 +1,5 @@
 from collections import Counter
 class Solution:
     def uniqueOccurrences(self, arr: List[int]) -> bool:
-        arr = Counter(Counter(arr).values()).values()
-        for i in arr:
-            if i != 1: return False
-        return True
-        
+        return sorted(list(Counter(Counter(Counter(arr).values()).values()).keys()))[-1] == 1
             
