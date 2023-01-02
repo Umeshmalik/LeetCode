c45e6cabe033321618cd2a0fc921747ac8955927
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-01-02 18:31:43 +0530

    Time: 40 ms (67.94%), Space: 13.9 MB (56.30%) - LeetHub

--- 0520-detect-capital/0520-detect-capital.py
+++ 0520-detect-capital/0520-detect-capital.py
@@ -2,8 +2,8 @@ class Solution:
     def detectCapitalUse(self, word: str) -> bool:
         isCap = isSmall = False
         for i in range(1, len(word)):
-            if 'A' <= word[i] <= 'Z':
+            if word[i].isupper():
                 isCap = True
-            if 'a' <= word[i] <= 'z':
+            else:
                 isSmall = True
         return False if isCap and (isSmall or 'a' <= word[0] <= 'z') else True
