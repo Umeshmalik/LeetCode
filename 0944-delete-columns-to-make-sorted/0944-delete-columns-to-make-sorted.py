8c9e1865e6569b60121f60b99595de3ce0277348
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-01-03 10:53:05 +0530

    Time: 214 ms (63.37%), Space: 14.7 MB (61.89%) - LeetHub

--- 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.py
@@ -1,15 +1,7 @@
 class Solution:
     def minDeletionSize(self, strs: List[str]) -> int:
-        arr = []
-        for j in range(len(strs[0])):
-            s = []
-            for i in range(len(strs)):
-                s.append(strs[i][j])
-            arr.append("".join(s))
         ans = 0
-        for i in arr:
-            for j in range(1, len(i)):
-                if i[j-1] > i[j]:
-                    ans += 1
-                    break
+        for i in zip(*strs):
+            if list(i) != sorted(i):
+                ans += 1
         return ans
