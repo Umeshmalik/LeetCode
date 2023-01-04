d3b77cbb4b4a5d6ec9f33ac5e25a27a5196cc2c7
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-01-04 19:22:09 +0530

    Time: 2397 ms (38.20%), Space: 28.3 MB (60.98%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -5,10 +5,7 @@ class Solution:
         count = 0
         for value in mp.values():
             if value == 1: return -1
-            if value%3 == 0:
-                count += value//3
-            elif value%3 == 1:
-                count += (1 if value == 2 else 2) + (value-4)//3
-            else:
-                count += 1 + (value-2)//3
+            if value%3 == 0: count += value//3
+            elif value%3 == 1: count += (1 if value == 2 else 2) + (value-4)//3
+            else: count += 1 + (value-2)//3
         return count
