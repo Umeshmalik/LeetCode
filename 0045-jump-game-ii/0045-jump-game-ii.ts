1498a0fe0129042627f6ec7172034074e2f40bc6
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-02-08 11:33:53 +0530

    Time: 57 ms (100.00%), Space: 45.4 MB (41.94%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.ts
+++ 0045-jump-game-ii/0045-jump-game-ii.ts
@@ -1,6 +1,6 @@
 function jump(nums: number[]): number {
-    let len = nums.length - 1, curr = -1, next = 0, ans = 0
-    for (let i = 0; next < len; i++) {
+    let curr = -1, next = 0, ans = 0
+    for (let i = 0; next < nums.length-1; i++) {
         if (i > curr) ans++, curr = next
         next = Math.max(next, nums[i] + i)
     }
