6ae563a2d634cae1f7a3af85637474682e34cca7
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-02-14 09:46:40 +0530

    Time: 44 ms (26.37%), Space: 13.8 MB (63.05%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -1,28 +1,28 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         carry = 0
-        ans = []
+        ans = ""
         lenA = len(a)-1
         lenB = len(b)-1
         while lenA >= 0 and lenB >= 0:
             sm = int(a[lenA]) + int(b[lenB]) + carry
-            ans.insert(0, str(sm%2))
+            ans  = str(sm%2) + ans
             carry = sm//2
             lenA -= 1
             lenB -= 1
 
         while lenA >= 0:
             sm = int(a[lenA]) + carry
-            ans.insert(0, str(sm%2))
+            ans = str(sm%2) + ans
             carry = sm//2
             lenA -= 1
 
         while lenB >= 0:
             sm = int(b[lenB]) + carry
-            ans.insert(0, str(sm%2))
+            ans = str(sm%2) + ans
             carry = sm//2
             lenB -= 1
 
         if carry == 1:
-            ans.insert(0, "1")
-        return "".join(ans)
+            ans = "1" + ans
+        return ans
