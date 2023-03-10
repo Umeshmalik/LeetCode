7527b2ad2d7528b78f6153a0f29a9dddbc2c951c
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-03-10 09:51:30 +0530

    Time: 98 ms (72.73%), Space: 49.7 MB (100.00%) - LeetHub

---
+++ 0382-linked-list-random-node/0382-linked-list-random-node.ts
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+class Solution {
+    arr: Array<number>;
+    constructor(head: ListNode | null) {
+        this.arr = []
+        while(head){
+            this.arr.push(head.val)
+            head = head.next
+        }
+    }
+
+    getRandom(): number {
+        return this.arr[Math.floor(Math.random() * this.arr.length)]
+    }
+}
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * var obj = new Solution(head)
+ * var param_1 = obj.getRandom()
+ */
