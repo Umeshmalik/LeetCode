10fbba866e352ccfb80d90fa0a7fca8c72003495
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-03-14 09:02:30 +0530

    Time: 29 ms (84.93%), Space: 13.8 MB (95.89%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -9,9 +9,5 @@ class Solution:
         def rec(node, arr=[]):
             if not node: return []
             if not node.left and not node.right: return [str(node.val)]
-            arr1 = rec(node.left, arr)
-            arr2 = rec(node.right, arr)
-            arr = [str(node.val) + i for i in arr1] + [str(node.val) + i for i in arr2]
-            return arr
-        arr = rec(root)
-        return sum([int(i) for i in arr])
+            return [str(node.val) + i for i in rec(node.left, arr)] + [str(node.val) + i for i in rec(node.right, arr)]
+        return sum([int(i) for i in rec(root)])
