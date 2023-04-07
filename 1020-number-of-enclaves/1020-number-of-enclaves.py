32de0cbec16c054571dbc992734ba91bc1fb5a63
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-04-07 10:58:14 +0530

    Time: 750 ms (45.05%), Space: 104.9 MB (5.60%) - LeetHub

---
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -0,0 +1,24 @@
+class Solution:
+    def numEnclaves(self, grid: List[List[int]]) -> int:
+        def reach(x, y):
+            if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == 0: return
+            grid[x][y] = 0
+            mt = [[0, -1], [-1, 0], [1, 0], [0, 1]]
+            for i, j in mt:
+                reach(x+i, y+j)
+        
+        for i in range(len(grid)):
+            reach(i, 0)
+            reach(i, len(grid[0]) - 1)
+        
+        for i in range(len(grid[0])):
+            reach(0, i)
+            reach(len(grid) - 1, i)
+        
+        count = 0
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    count += 1
+                    grid[i][j] = 0
+        return count
