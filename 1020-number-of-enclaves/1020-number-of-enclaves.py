618ec907ff5b0f588c275b0187d9ee1b85c7fa26
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-04-07 11:02:34 +0530

    Time: 731 ms (49.17%), Space: 105 MB (5.60%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.py
+++ 1020-number-of-enclaves/1020-number-of-enclaves.py
@@ -4,8 +4,7 @@ class Solution:
             if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) or grid[x][y] == 0: return
             grid[x][y] = 0
             mt = [[0, -1], [-1, 0], [1, 0], [0, 1]]
-            for i, j in mt:
-                reach(x+i, y+j)
+            for i, j in mt: reach(x+i, y+j)
         
         for i in range(len(grid)):
             reach(i, 0)
@@ -17,6 +16,5 @@ class Solution:
         
         count = 0
         for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                count += grid[i][j]
+            for j in range(len(grid[0])): count += grid[i][j]
         return count
