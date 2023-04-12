2d3db96d1e1737e33354da7470238436530a2095
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-04-12 10:05:27 +0530

    Time: 65 ms (100.00%), Space: 43.1 MB (100.00%) - LeetHub

--- 2621-sleep/2621-sleep.ts
+++ 2621-sleep/2621-sleep.ts
@@ -2,8 +2,8 @@ async function sleep(millis: number): Promise<void> {
     return new Promise((resolve, reject) => {
         setTimeout(()=> {
             resolve();
-        }, millis)
-    })
+        }, millis);
+    });
 }
 
 
