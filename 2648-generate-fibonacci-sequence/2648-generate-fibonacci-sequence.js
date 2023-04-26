5ce7235d783a690c1f32897ec2e39422e77990fc
Umesh <42288819+Umeshmalik@users.noreply.github.com>
2023-04-26 16:14:14 +0530

    Time: 57 ms (61.01%), Space: 41.5 MB (93.53%) - LeetHub

---
+++ 2648-generate-fibonacci-sequence/2648-generate-fibonacci-sequence.js
@@ -0,0 +1,19 @@
+/**
+ * @return {Generator<number>}
+ */
+var fibGenerator = function*() {
+    let a = 0, b = 1;
+    yield a
+    while(true){
+        let k = a + b
+        b = a
+        a = k
+        yield a
+    }
+};
+
+/**
+ * const gen = fibGenerator();
+ * gen.next().value; // 0
+ * gen.next().value; // 1
+ */
